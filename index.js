444218be884d5c3a774c6ce55e0927d19b5e50de
Aldo Danilo Vargas Esquivel <el1n53ct054ur103000@gmail.com>
2024-10-27 23:33:25 -0600

    Primera parte de la documentación inicial terminada, y estructura del cubo creada y establecida para un mejor desarrollo de la lógica en la resolución y ejecución

--- index.js
+++ index.js
@@ -40,18 +40,104 @@
 
     // Para los colores usaré las siguientes claves:
 
-                       // Amarillo - AM
-                      // Blanco   - BL
-                     // Verde    - VE
-                    // Azul     - AZ
-                   // Rojo     - RO
-                  // Naranja  - NA
+    //     Amarillo - AM
+    //     Blanco   - BL
+    //     Verde    - VE
+    //     Azul     - AZ
+    //     Rojo     - RO
+    //     Naranja  - NA
 
-    // Y cada posición del cubo tendrá un nombre específico, este nombre serán letras del abecedario según
-   // el orden que me aprendí para armarlo con el metodo que se usa para armarlo a ciegas
+     // Y cada posición del cubo tendrá un nombre específico, este nombre serán letras del abecedario según
+    // el orden que me aprendí para armarlo con el metodo que se usa para armarlo a ciegas
+
+     // Para las aristas se usarán letras mayúsculas:
+    // A B C D E F G H I J L M N O P Q R S T U V
+   
+     // Para las esquinas se usarán letras minúsculas:
+    // a b c d e f g h i j k l m n o p q r s t u
+
+    // El cubo siempre estará orientado con la cara amarilla arriba y la verde enfrente:
+
+    // ╩ ╦ ╠ ═ ╬ ╣ ║ ╗ ╝ ╚ ╔ ¬ │ ┤ ┤ ┐ └ ┴ ┬ ├ ─ ┼ 
+
+    //                                  ╔══════════╗
+    //                                  ║          ║
+    //                                  ║    AM    ║
+    //                                  ║          ║
+    //            ╔══════════╦══════════╬══════════╬══════════╗
+    //            ║          ║          ║          ║          ║
+    //            ║    AZ    ║    RO    ║    VE    ║    NA    ║
+    //            ║          ║          ║          ║          ║
+    //            ╚══════════╩══════════╬══════════╬══════════╝
+    //                                  ║          ║
+    //                                  ║    BL    ║
+    //                                  ║          ║
+    //                                  ╚══════════╝
+
+    // Las posiciónes del cubo:
+
+    //                                                ╔═════╦═════╦═════╗
+    //             ▓▓▓ - BUFFER principal             ║ ▓▓▓ |  E  |  b  ║
+    //                                                ╠─────┼─────┼─────╣
+    //             ▒▒▒ - Complemento de BUFFER        ║  A  | AMA | ▓▓▓ ║
+    //                                                ╠─────┼─────┼─────╣
+    //                                                ║  c  |  C  |  a  ║
+    //            ╔═════╦═════╦═════╦═════╦═════╦═════╬═════╬═════╬═════╬═════╦═════╦═════╗
+    //            ║  j  |  F  | ▒▒▒ ║ ▒▒▒ |  B  |  o  ║  h  |  D  |  i  ║  p  | ▒▒▒ |  q  ║
+    //            ╠─────┼─────┼─────╬─────┼─────┼─────╬─────┼─────┼─────╬─────┼─────┼─────╣
+    //            ║  L  | AZU |  M  ║  N  | ROJ |  G  ║  H  | VER |  I  ║  J  | NAR |  K  ║
+    //            ╠─────┼─────┼─────╬─────┼─────┼─────╬─────┼─────┼─────╬─────┼─────┼─────╣
+    //            ║  m  |  U  |  n  ║  r  |  O  |  s  ║  k  |  Q  |  l  ║  t  |  S  |  u  ║
+    //            ╚═════╩═════╩═════╩═════╩═════╩═════╬═════╬═════╬═════╬═════╩═════╩═════╝
+    //                                                ║  d  |  R  |  e  ║
+    //              El BUFFER es la pieza que         ╠─────┼─────┼─────╣
+    //              funcionará como sensor para       ║  P  | BLA |  T  ║
+    //              saber que pieza se debe           ╠─────┼─────┼─────╣
+    //              acomodar                          ║  g  |  V  |  f  ║
+    //                                                ╚═════╩═════╩═════╝
+
+    // El indice de cada posición de cada cara en la matriz se define de la siguiente manera:
+
+    //
+    //    ╔═════╦═════╦═════╗
+    //    ║  0  |  1  |  2  ║    AM: [              Estaos indicando el indice de las posiciones
+    //    ╠─────┼─────┼─────╣        0, 1, 2,       en la cara amarilla, sin embargo, se debe
+    //    ║  3  |  4  |  5  ║        3, 4, 5,       indicar el color que se encuentra en esa 
+    //    ╠─────┼─────┼─────╣        6, 7, 8        posición de acuerdo a las claves establecidas
+    //    ║  6  |  7  |  8  ║    ]                  al inicio.
+    //    ╚═════╩═════╩═════╝
+    //
+
 
 let EstadoInicial = {
-    CARA_AMARILLA: [
-
+    AM: [
+        'AM','AM','AM',
+        'AM','AM','AM',
+        'AM','AM','AM'
+    ],
+    BL: [
+        'BL','BL','BL',
+        'BL','BL','BL',
+        'BL','BL','BL'
+    ],
+    VE: [
+        'VE','VE','VE',
+        'VE','VE','VE',
+        'VE','VE','VE'
+    ],
+    AZ: [
+        'AZ','AZ','AZ',
+        'AZ','AZ','AZ',
+        'AZ','AZ','AZ'
+    ],
+    RO: [
+        'RO','RO','RO',
+        'RO','RO','RO',
+        'RO','RO','RO'
+    ],
+    NA: [
+        'NA','NA','NA',
+        'NA','NA','NA',
+        'NA','NA','NA'
     ]
 };
