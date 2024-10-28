e25f85a2c4943ee8c7b63cefb3621a50e282697d
Aldo Danilo Vargas Esquivel <el1n53ct054ur103000@gmail.com>
2024-10-27 21:38:16 -0600

    inicio del proyecto

---
+++ index.js
@@ -0,0 +1,57 @@
+
+
+      // He creado este código para resolver el cubo de Rubik´s 3x3, ten en cuenta que lo estoy creando completamente
+     // desde cero, usando mi propia lógica de programación y usando los algoritmos que para mí han sido los más
+    // sencillos de programar, por lo tanto no es el algoritmo más eficiente para la resolución, pero es funcional.
+
+      // Éste es un reto que ya he realizado antes, pero el resultado final no me gusto, aunque si funcionó completamente.
+     // Dejaré el enlace a los códigos anteriores, ya que lo hice en dos versiones, la primera recuerdo que fue en JS y la
+    // segunda en C++
+
+        // He optado por hacer esta versión en JS para poder publicarla en un sitio web, como parte de mi portafolio, donde
+       // podré añadir una interfáz gráfica bastante amigable y funcional, y dejando una nomenclatura de los movimientos
+      // bien establecida para poder integrar el programa con un robot que pueda leer la nomenclatura y resolver el cubo de
+     // Rubik´s, y que también el robot pueda proporcionar el estado del cubo al programa. Me he basado en uno de los métodos
+    // que se usan para armar el cubo de Rubik´s a ciegas, el metodo para BLIND.
+
+        // Dicho lo anterior, sólo queda comenzar con el código y la explicación del mismo
+
+       // Creaador: Aldo Danilo Vargas Esquivel
+      // Inicio del proyecto: 27/10/2024
+     // Fin del proyecto: --/--/----
+    // Repositorio de GitHub: https://github.com/Danilo-Vargas/Rubiks-Solver
+   
+        // Visíta mi sitio web https://danilovargas.tech para conocer más acerca de mí y los proyectos en los que he trabajado
+
+                  ////////////////////////////////////////////////////////////////////////////////////////////
+                 
+                   //////////    ///      ///  ///////////   ////////////  ///      ///  ///  ///////////
+                  ///     ////  ///      ///  ///     ////      ////      ///      ///  //  ///
+                 ///     ////  ///      ///  ///     ////      ////      ///    ////       ///
+                //////////    ///      ///  //////////        ////      /////////          /////////
+               ///    ////   ///      ///  ///     ////      ////      ///    ////                ///
+              ///      ///  ////    ////  ///     ////      ////      ///      ///              ////
+             ///      ///    ////////    ///////////   ////////////  ///      ///      ///////////
+
+        ////////////////////////////////////////////////////////////////////////////////////////////
+
+
+    // Iniciemos explicando la estructura digital del cubo que crearé
+
+    // Para los colores usaré las siguientes claves:
+
+                       // Amarillo - AM
+                      // Blanco   - BL
+                     // Verde    - VE
+                    // Azul     - AZ
+                   // Rojo     - RO
+                  // Naranja  - NA
+
+    // Y cada posición del cubo tendrá un nombre específico, este nombre serán letras del abecedario según
+   // el orden que me aprendí para armarlo con el metodo que se usa para armarlo a ciegas
+
+let EstadoInicial = {
+    CARA_AMARILLA: [
+
+    ]
+};
